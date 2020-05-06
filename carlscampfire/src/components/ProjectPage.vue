a9c97a2ec1d922428b1e2472068b211a9702d9f9
Carl-J-M <carlmensahmail@gmail.com>
2020-05-06 02:45:24 +0100

    Removed opacity from project cards

--- carlscampfire/src/components/ProjectPage.vue
+++ carlscampfire/src/components/ProjectPage.vue
@@ -132,7 +132,6 @@ padding: 0.1em;
   margin: 0em 1em;
   transition: all ease-in-out 0.4s;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  opacity: 0.4;
 }
 .card:hover {
   cursor: pointer;
